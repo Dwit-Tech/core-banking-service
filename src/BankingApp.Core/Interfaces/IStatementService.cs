1de34cc26c06c9bd0d736ee79ed7590115a00555
Benedict Okojie <Benedict.Okojie@azenia.com>
2024-02-07 23:21:45 +0100

    Implement get statement method

--- src/BankingApp.Core/Interfaces/IStatementService.cs
+++ src/BankingApp.Core/Interfaces/IStatementService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BankingApp.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace BankingApp.Core.Interfaces
 {
     public interface IStatementService
     {
+        Task<List<Statement>> GetStatements();
     }
 }
--- src/BankingApp.Core/Services/StatementService.cs
+++ src/BankingApp.Core/Services/StatementService.cs
@@ -1,4 +1,7 @@
 ﻿using BankingApp.Core.Interfaces;
+using BankingApp.Data;
+using BankingApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +12,16 @@ namespace BankingApp.Core.Services
 {
     public class StatementService : IStatementService
     {
+        private readonly StatementDbContext _statementDbContext;
+
+        public StatementService(StatementDbContext statementDbContext)
+        {
+            _statementDbContext = statementDbContext;
+        }
+
+        public async Task<List<Statement>> GetStatements()
+        {
+            return await _statementDbContext.Statements.ToListAsync();
+        }
     }
 }
