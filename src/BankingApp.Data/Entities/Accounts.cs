e5ddf9aa2a01402b2df0d1a8fbad0c5f6c52127d
Priscilla Okoro <oyomepriscilla@gmail.com>
2024-02-11 16:31:40 +0000

    create account class

---
+++ src/BankingApp.Data/Entities/Accounts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Data.Entities
+{
+    public class Accounts : BaseEntity
+    {
+        public string AccountNumber { get; set; }
+        public string AccountHolderName { get; set; }
+        public decimal Balance { get; set; }
+        public string AccountType { get; set; }
+        public int CustomerID { get; set; }
+    }
+}
