c56a781920dbac37ac5ba4af6702e9428414682a
Nwaogu Florence <crystalfloxy74@gmail.com>
2024-02-15 00:18:08 +0100

    Migration successful

---
+++ src/BankingApp.Data/Entities/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingApp.Data.Entities
+{
+    public class Transaction : BaseEntity
+    {
+        public int TransactionId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public TransactionType Type { get; set; }
+        public string Description { get; set; }
+
+        public enum TransactionType
+        {
+            Deposit,
+            Withdrawal
+        }
+    }
+}
