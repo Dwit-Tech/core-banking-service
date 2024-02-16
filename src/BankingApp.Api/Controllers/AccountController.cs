316e51b3462e7c41ad43dd566761614618e2c1be
user <user@KIKI>
2024-02-16 01:52:02 +0100

    get method done and dusted

--- src/BankingApp.Api/Controllers/AccountController.cs
+++ src/BankingApp.Api/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BankingApp.Core.Interfaces;
+using BankingApp.Data.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BankingApp.Api.Controllers
@@ -6,7 +8,23 @@ namespace BankingApp.Api.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
-    { 
+    {
+        private readonly IAccountService _accountService;
+         public AccountController(IAccountService accountService)
+        {
+          _accountService = accountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get() 
+        {
+            List<Account> accounts = await _accountService.GetAccounts();
+            if (accounts == null || accounts.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(accounts);
+        }
     }
 }
 
--- src/BankingApp.Core/Services/AccountService.cs
+++ src/BankingApp.Core/Services/AccountService.cs
@@ -1,4 +1,7 @@
 ﻿using BankingApp.Core.Interfaces;
+using BankingApp.Data;
+using BankingApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +12,14 @@ namespace BankingApp.Core.Services
 {
     public class AccountService : IAccountService
     {
+        private readonly AccountDbContext _accountDbContext;
+        public AccountService(AccountDbContext accountDbContext)
+        {
+            _accountDbContext = accountDbContext;   
+        }
+        public async Task<List<Account>> GetAccounts()
+        {
+            return await _accountDbContext.Accounts.ToListAsync();
+        }
     }
 }
