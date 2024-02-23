8179f9737e4d4a01f950dcad9a0d8af4a798bd61
Nwaogu Florence <crystalfloxy74@gmail.com>
2024-02-23 23:29:26 +0100

    Get method for customer successful

--- src/BankingApp.Core/Interfaces/ICustomerService.cs
+++ src/BankingApp.Core/Interfaces/ICustomerService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BankingApp.Data.Entities;
 
 namespace BankingApp.Core.Interfaces
 {
--- src/BankingApp.Data/Entities/Customer.cs
+++ src/BankingApp.Data/Entities/Customer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BankingApp.Data.Entities;
+
 
 namespace BankingApp.Data.Entities
 {
